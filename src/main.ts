aff6ca97811411326e2165577584594ebbe851fb
jingwei950 <jingwei950@hotmail.sg>
2024-08-25 11:24:25 +0800

    added on swap function

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,25 @@ import { createSwapy } from 'swapy';
   standalone: true,
   template: `
     <div #container class="container grid grid-cols-3 p-2 gap-2">
-  <div class="col-span-2 bg-gray-300 rounded-lg" data-swapy-slot="foo">
-    <div class="w-full h-full p-2 rounded-lg bg-red-500" data-swapy-item="a">
+  <div class="col-span-2 bg-gray-300 rounded-lg" data-swapy-slot="1">
+    <div class="w-full h-full p-2 rounded-lg bg-red-500" data-swapy-item="content1">
       content 1
     </div>
   </div>
 
-  <div class="col-span-1 bg-gray-300 rounded-lg" data-swapy-slot="bar">
-    <div class="w-full h-full p-2 rounded-lg bg-green-500 " data-swapy-item="b">
+  <div class="col-span-1 bg-gray-300 rounded-lg" data-swapy-slot="2">
+    <div class="w-full h-full p-2 rounded-lg bg-green-500 " data-swapy-item="content2">
       content 2
     </div>
   </div>
 
-  <div class="col-span-1 bg-gray-300 rounded-lg" data-swapy-slot="baz">
-    <div class="w-full h-full p-2 rounded-lg bg-blue-500" data-swapy-item="c">
+  <div class="col-span-1 bg-gray-300 rounded-lg" data-swapy-slot="3">
+    <div class="w-full h-full p-2 rounded-lg bg-blue-500" data-swapy-item="content3">
       content 3
     </div>
   </div>
-  <div class="col-span-2 bg-gray-300 rounded-lg" data-swapy-slot="asd">
-    <div class="w-full h-full p-2 rounded-lg bg-yellow-500 " data-swapy-item="d">
+  <div class="col-span-2 bg-gray-300 rounded-lg" data-swapy-slot="4">
+    <div class="w-full h-full p-2 rounded-lg bg-yellow-500 " data-swapy-item="content4">
       content 4
     </div>
   </div>
@@ -43,6 +43,13 @@ export class App {
     });
     // You can disable and enable it anytime you want
     swapy.enable(true);
+
+    // On Swap
+    swapy.onSwap((event) => {
+      console.log(event.data.object);
+      console.log(event.data.array);
+      console.log(event.data.map);
+    })
   }
 }
 
